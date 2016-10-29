4b11bd3045105a466a591dd85934d6f236086ac2
Daniel Waltrip <dwaltrip77@gmail.com>
2016-10-28 17:14:02 -0700

    Move app-state-inspector out as its own NPM module

--- www/js/init.js
+++ www/js/init.js
@@ -2,6 +2,9 @@ import m from 'mithril';
 import db from 'db';
 import routes from 'routes';
 
-
 // start app
 m.route(document.getElementById('app'), '/', routes);
+
+// for debugging only
+import Inspector from 'app-state-inspector';
+Inspector.mount();
